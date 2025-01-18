136c39bb331770c9db5ee21b16fd2c50b32632a7
abraham <abrahammathewos.meja.5695@student.uu.se>
2025-01-18 13:14:30 -0600

    Initail commit

---
+++ src/main/java/net/javaguides/microserviceswithjava/MicroservicesWithJavaApplication.java
@@ -0,0 +1,13 @@
+package net.javaguides.microserviceswithjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesWithJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroservicesWithJavaApplication.class, args);
+    }
+
+}
